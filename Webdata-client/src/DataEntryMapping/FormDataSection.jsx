c4c7a969bb1c8a1ecb1683cc564a60ef66e79f6c
Sparsh-Jain-531 <sparshjain531@gmail.com>
2025-03-12 12:08:51 +0530

    add data entry mapping components for enhanced data handling

---
+++ Webdata-client/src/DataEntryMapping/FormDataSection.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from "react";
+
+const FormDataEntrySection = ({ data }) => {
+  const [formData, setFormData] = useState([]);
+
+  useEffect(() => {
+    setFormData(Array.isArray(data.formdata) ? data.formdata : [data.formdata]);
+  }, [data]);
+
+  return (
+    <div className="border-e lg:w-3/12 xl:w-[20%] order-lg-1">
+      <div className="overflow-hidden w-[100%]">
+        <article
+          style={{ scrollbarWidth: "thin" }}
+          className="py-10 mt-5 lg:mt-16 shadow transition hover:shadow-lg mx-auto overflow-y-auto lg:h-[80vh] rounded-lg flex flex-row lg:flex-col lg:items-center w-[95%] bg-blue-500"
+        >
+          {Array.isArray(formData) && formData.length > 0 && formData[0] ? (
+            Object.entries(formData[0]).map(([key, value], index) => (
+              <div
+                key={index}
+                className="w-5/6 px-3 lg:px-0 py-1 overflow-x font-bold justify-center items-center"
+              >
+                <label className="w-full overflow-hidden rounded-md font-semibold py-2 shadow-sm">
+                  <span className="text-sm text-white font-bold flex">
+                    {key}
+                  </span>
+                </label>
+                <input
+                  type="text"
+                  value={value || ""}
+                  readOnly
+                  className="mt-1 border-none p-2 focus:border-transparent text-center rounded-lg focus:outline-none focus:ring-0 sm:text-sm w-48"
+                />
+              </div>
+            ))
+          ) : (
+            formData.length<=0 ? <div className="text-white">No Data Found</div> : <div className="text-white">Loading..</div>
+          )}
+
+          {/* <div className="w-5/6 px-3 lg:px-0 py-1 overflow-x font-bold justify-center items-center">
+            <label className="w-full overflow-hidden rounded-md font-semibold py-2 shadow-sm">
+              <span className="text-sm text-white font-bold flex">data 1</span>
+            </label>
+            <input
+              type="text"
+              className={`mt-1 border-none p-2 focus:border-transparent text-center rounded-lg focus:outline-none focus:ring-0 sm:text-sm w-48`}
+            />
+          </div> */}
+        </article>
+      </div>
+    </div>
+  );
+};
+
+export default FormDataEntrySection;
---
+++ Webdata-client/src/DataEntryMapping/QuestionDataEntrySection.jsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from "react";
+
+const QuestionDataEntrySection = ({ data }) => {
+
+  const [questionData, setQuestionData] = useState([]);
+
+  useEffect(() => {
+    setQuestionData(
+      Array.isArray(data.questionData) ? data.questionData : [data.questionData]
+    );
+  }, [data]);
+
+  return (
+    <div className="w-full xl:w-2/3 xl:px-6 mx-auto text-white">
+      <div className="my-4 w-full ">
+        <label
+          className="text-xl font-semibold ms-2 leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+          htmlFor="questions"
+        >
+          Questions:
+        </label>
+        <div
+          className="flex overflow-auto max-h-[360px] mt-3 ms-2 xl:ms-2 flex-wrap"
+          style={{ scrollbarWidth: "thin" }}
+        >
+          {Array.isArray(questionData) &&
+          questionData.length > 0 &&
+          questionData[0] ? (
+            Object.entries(questionData[0]).map(([key, value], index) => {
+                let red=false;
+                if(value === " " || value === "*"){
+                    red=true;
+                }
+              return (
+                <div key={index} className="flex">
+                <div className=" me-3 my-1 flex">
+                  <label className="font-bold text-sm w-9 text-bold my-1">
+                    {key}
+                  </label>
+                  <div className="flex rounded">
+                    <input
+                      type="text"
+                      value={value}
+                      className={`h-7 w-7 text-center text-black rounded text-sm ${red ? "bg-red-500" : ""} `}
+                    />
+                  </div>
+                </div>
+              </div>
+              )
+            })
+          ) : questionData.length <= 0 ? (
+            <div className="text-white">No Data Found</div>
+          ) : (
+            <div className="text-white">Loading..</div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default QuestionDataEntrySection;
